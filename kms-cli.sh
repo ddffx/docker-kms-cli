3da99b4b2be6761b45ab6a75ad503593eb717f6a
Deb Das <debabrata.das@gmail.com>
2018-04-04 16:35:02 -0400

    Update kms-cli.sh
    update to kms-cli script

--- kms-cli.sh
+++ kms-cli.sh
@@ -1,2 +1,15 @@
-#!/bin/bash
-docker run --rm --env-file $HOME/.kms-env ddffx/kms-cli /usr/local/bin/kms-cli "$@"
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# enable interruption signal handling
+trap - INT TERM
+
+docker run --rm \
+	-t $(tty &>/dev/null && echo "-i") \
+  -v "$HOME/.aws:/root/.aws" \
+	-v "$(pwd):/project" \
+	ddffx/kms-cli \
+	"$@"
